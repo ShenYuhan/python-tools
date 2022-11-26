c4ee1b7d0262ff21006ca1bec4da07dfa68db93e
codefeather <yhan_shen@163.com>
2022-11-26 12:11:46 +0800

    增加跳转qq群

---
+++ 3_kun_keyboard_catcher/readme.md
@@ -0,0 +1,5 @@
+编译方法  
+进入到3_kun_keyboard_catcher/tools/中  
+执行`pyi-makespec -F -i .\imgs\kun_keyboard.ico -w .\qt_util.py`以生成spec文件  
+修改spec文件中`datas=[('audios','audios'),('imgs','imgs')],`一行，audios，imgs为主函数所在文件同级目录  
+修改完之后执行`pyinstaller .\qt_util.spec`就可以生成可执行文件  
--- 3_kun_keyboard_catcher/tools/qt_util.py
+++ 3_kun_keyboard_catcher/tools/qt_util.py
@@ -1,4 +1,5 @@
 #coding:utf-8
+import functools
 import sys
 import os
 from PyQt5 import QtGui, QtWidgets, QtCore
@@ -6,16 +7,13 @@ import monitor
 from pynput.keyboard import Key
 import playsound
 import threading
-import urllib.request
 
-
+# 得到当前执行文件同级目录的其他文件绝对路径
 def resource_path(relative_path):
     if getattr(sys, 'frozen', False):
-        # print("1111")
         base_path = sys._MEIPASS
     else:
         base_path = os.path.abspath('.')
-    # print("base_path = ", base_path)
     return os.path.join(base_path, relative_path)
 
 
@@ -33,6 +31,7 @@ class MainWidgets(QtWidgets.QWidget):
         # 定时器，用于长时间不输入清空输入状态和闭嘴
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.reset_char)
+        # 3秒定时清除文字
         self.timer.start(3000)
 
         # 字母与音频对应关系
@@ -47,53 +46,41 @@ class MainWidgets(QtWidgets.QWidget):
             'T': resource_path(os.path.join("audios", "t.mp3")),
             'M': resource_path(os.path.join("audios", "m.mp3")),
             'jntm': resource_path(os.path.join("audios", "ngm.mp3"))
-            # 'n': os.path.join(os.path.dirname(os.path.abspath(__file__)), "audios", "n.mp3"),
-            # 't': os.path.join(os.path.dirname(os.path.abspath(__file__)), "audios", "t.mp3"),
-            # 'm': os.path.join(os.path.dirname(os.path.abspath(__file__)), "audios", "m.mp3"),
-            # 'J': os.path.join(os.path.dirname(os.path.abspath(__file__)), "audios", "j.mp3"),
-            # 'N': os.path.join(os.path.dirname(os.path.abspath(__file__)), "audios", "n.mp3"),
-            # 'T': os.path.join(os.path.dirname(os.path.abspath(__file__)), "audios", "t.mp3"),
-            # 'M': os.path.join(os.path.dirname(os.path.abspath(__file__)), "audios", "m.mp3")
         }
         self.init_monitor()
         
         self.windowinit()
         self.icon_quit()
 
+    # 版本提示
+    def version_content(self):
+        QtWidgets.QMessageBox.information(self, "坤音键盘v1.0", "抖音/B站：走神的阿圆 QQ群:766402914",
+                                QtWidgets.QMessageBox.StandardButton.Yes)
+
+    # 弹出提示
     def open_qq_group(self):
-        QtWidgets.QMessageBox.information(self, "走神的阿圆", "抖音/B站：走神的阿圆 QQ群:766402914",
-                            QtWidgets.QMessageBox.StandardButton.Yes)
-        urllib.request.urlopen("https://jq.qq.com/?_wv=1027&k=A1DK3FhW")
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl('https://jq.qq.com/?_wv=1027&k=A1DK3FhW'))
 
     # 松开键盘时
     def on_release(self, key):
+        # self.lab_content.setText("")
+        # self.lab_content.adjustSize()
         # 闭嘴
         self.lab.setPixmap(QtGui.QPixmap(resource_path(os.path.join("imgs", "cai1.png"))))
-        # try:
-            # print("alphanueric jkey {} pressed".format(key.char))
-        # except AttributeError:
-            # print("spcial key {0} pressed".format(key))
 
     # 摁下键盘时
     def on_press(self, key):
-        # print("{0} released".format(key))
-        if key == Key.esc:
-            # esc停止监测
-            return False
         try:
             ch = key.char
         except AttributeError:
             ch = key.name
         self.set_char(ch)
 
-    def play_ngm(self):
-        t = threading.Thread(target=self.play_radio, args=(self.ch2audio["jntm"],))
-        t.start()
 
     def init_monitor(self):
         hot_keys_func_map = {
-            # "<ctrl>+j": functools.partial(self.play_radio, path=self.ch2audio["jntm"])
-            "<ctrl>+j": self.play_ngm
+            "<ctrl>+j": functools.partial(self.play_radio, path=self.ch2audio["jntm"])
+            # "<ctrl>+j": self.play_ngm
         }
         self.monitor = monitor.KeyBoardLister(on_press_func=self.on_press, on_release_func=self.on_release, hot_keys_func_map=hot_keys_func_map)
     
@@ -116,7 +103,6 @@ class MainWidgets(QtWidgets.QWidget):
         self.lab_content.setFont(font)
         self.lab_content.setStyleSheet("color:black;")
         self.lab_content.move(38, 28)
-        # self.lab_content.setText("123456")
 
         # 气泡框
         self.lab_bubble.move(0, 0)
@@ -133,17 +119,16 @@ class MainWidgets(QtWidgets.QWidget):
         self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground, True)
         self.show()
 
+    # 开线程放音乐，避免阻断主流程，实现可以同时放多个radio
     def play_radio(self, path):
-        # print("path = ", path)
-        playsound.playsound(path)
+        t = threading.Thread(target=playsound.playsound, args=(path,))
+        t.start()
     
     def set_char(self, ch):
         if ch is None:
             return
         if ch in self.ch2audio:
-            t = threading.Thread(target=self.play_radio, args=(self.ch2audio[ch],))
-            # self.play_radio(self.ch2audio[ch])
-            t.start()
+            self.play_radio(self.ch2audio[ch])
         if ch == "j" or ch == "J":
             ch = "只因"
 
@@ -174,8 +159,7 @@ class MainWidgets(QtWidgets.QWidget):
 
     # 长时间没有触发则要回归到最初状态
     def reset_char(self):
-        # print("reset char")
-        # 设置字母
+        # 清除文字
         self.lab_content.setText("")
         self.lab_content.adjustSize()
         # 闭嘴
@@ -204,7 +188,9 @@ class MainWidgets(QtWidgets.QWidget):
         tpMenu = QtWidgets.QMenu(self) 
         # 为菜单指定一个选项
 
-        quit_menu_qq = QtWidgets.QAction('更多', self, triggered=self.open_qq_group)
+        version_menu =  QtWidgets.QAction('作者', self, triggered=self.version_content)
+        tpMenu.addAction(version_menu)
+        quit_menu_qq = QtWidgets.QAction('QQ群', self, triggered=self.open_qq_group)
         tpMenu.addAction(quit_menu_qq)
         quit_menu = QtWidgets.QAction('退出', self, triggered=self.quit)
         tpMenu.addAction(quit_menu)
